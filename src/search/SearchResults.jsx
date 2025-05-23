81b4a7266e8c8c7d953392a5db25481e530fc281
IvanLeong03 <quantumoopsie@gmail.com>
2025-05-23 14:24:21 +0100

    add: sort by newest or oldest for all search results

--- src/search/SearchResults.jsx
+++ src/search/SearchResults.jsx
@@ -8,7 +8,19 @@ function SearchResults() {
     <div className="p-4 w-[80dvw] mx-auto my-16">
       <h1 className="text-2xl mb-8">Search Results for: "{query}"</h1>
       {/* Render your search results here based on `query` */}
-      <ul className="flex flex-col gap-8">
+      
+
+      <div className="mb-6">
+        <label htmlFor="sort" className="mr-2 font-medium">
+          Sort by:
+        </label>
+        <select id="sort" name="sort" className="border rounded px-2 py-1">
+          <option value="newest">Newest</option>
+          <option value="oldest">Oldest</option>
+        </select>
+      </div>
+
+      <ul className="flex flex-col gap-16 text-lg">
         <li>Search Result 1</li>
         <li>Search Result 2</li>
         <li>Search Result 3</li>
