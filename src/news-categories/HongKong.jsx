5d541d8c4b688b9f5fcad03750a89b4dd7acd70c
IvanLeong03 <quantumoopsie@gmail.com>
2025-06-27 11:14:22 +0800

    fix: same sizing rules applied to the other 3 main pages

--- src/news-categories/HongKong.jsx
+++ src/news-categories/HongKong.jsx
@@ -100,17 +100,24 @@ function HongKong() {
     return (
         <div className="flex flex-col w-[80dvw] mx-auto my-16">
             <h1 className="text-5xl font-bold my-4 pl-5">{language === "zh-Hant" ? "香港" : language === "zh-Hans" ? "香港" : "HONG KONG"}</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {articles.map((article, index) => (
-                    <div key={index} className="flex flex-col rounded-lg p-4 m-2">
+                    <div
+                        key={index}
+                        className={
+                            index === 0
+                                ? "col-span-1 md:col-span-2 lg:col-span-2 row-span-1 flex flex-col rounded-lg p-4 m-2 lg:h-[32rem]"
+                                : "flex flex-col rounded-lg p-4 m-2"
+                        }
+                        style={index === 0 ? { minHeight: '20rem' } : {}}
+                    >
                         <div className="w-full aspect-[16/9] overflow-hidden border border-[var(--color-line-grey)]">
                             <img
                                 src={article.imageUrl}
-                                alt={article.title}                                
+                                alt={article.title}
                                 className="object-cover w-full h-full"
                             />
                         </div>
-                        
                         <div className="w-1/2 my-4">
                             <SplitBar cPercent={article.cPercent} pPercent={article.pPercent} />
                         </div>
