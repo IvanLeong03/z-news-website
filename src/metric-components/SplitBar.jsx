87c62d5152576249401b1295f96a902cf0655814
IvanLeong03 <quantumoopsie@gmail.com>
2025-07-30 17:38:08 +0800

    fix: moved conservative to liberal labels out of the bar

--- src/metric-components/SplitBar.jsx
+++ src/metric-components/SplitBar.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 
 function SplitBar({ cPercent = 50, liberalPercent = 50 }) {
   return (
-    <div className="flex w-full overflow-auto whitespace-nowrap shadow-sm text-[0.5rem] text-[var(--color-gs-black)]">
+    <div className="flex flex-col w-full overflow-auto whitespace-nowrap text-xs text-[var(--color-gs-black)]">
 
-      <div
+      <p>Conservative</p>
+      {/* bar */}
+      <div className='w-full flex text-sm shadow-sm font-semibold'>
+        <div
         className="flex items-center justify-center bg-[var(--color-bg-grey)] px-1"
         style={{ width:`${cPercent}%` }}
       >
-        Conservative: {cPercent}%
+          {cPercent}%
+        </div>
+
+        <div
+          className="flex items-center justify-center bg-[var(--color-secondary-3)] px-1"
+          style={{ width:`${liberalPercent}%` }}
+        >
+          {liberalPercent}%
+        </div>
+      </div>
+      <div className='w-full flex justify-end'>
+        <p>Liberal</p>
       </div>
 
-      <div
-        className="flex items-center justify-center bg-[var(--color-secondary-3)] px-1"
-        style={{ width:`${liberalPercent}%` }}
-      >
-        Liberal: {liberalPercent}%
-      </div>
-      
     </div>
   );
 }
